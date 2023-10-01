55dbfa6e67dad21d7e07439326d8ba71485692fe
Kamil Mugutdinov <mugukamil@gmail.com>
2023-10-01 23:15:45 +0300

    unoptimize images, style form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,22 +47,24 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
 
 function FeedbackForm() {
     return (
-        <form name="feedback-form" method="post" data-netlify="true">
+        <form name="feedback-form" method="post" data-netlify="true" className="text-black">
             <input type="hidden" name="form-name" value="feedback-form" />
-            <p>
+            <p className="mb-2">
                 <label htmlFor="">
                     Your name
-                    <input type="text" name="name" />
+                    <input type="text" name="name" className="border border-gray-300" />
                 </label>
             </p>
-            <p>
+            <p className="mb-2">
                 <label htmlFor="">
                     Feedback
-                    <input type="text" name="feedback" />
+                    <input type="text" name="feedback" className="border border-gray-300" />
                 </label>
             </p>
             <p>
-                <button>Submit</button>
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Submit
+                </button>
             </p>
         </form>
     );
